b9deff2f42565456cad879524ef59cb8982673e4
Jayasri 1927 <jayasrig2002@gmail.com>
2024-12-12 14:39:18 +0530

    CHNAGES

--- password-reset/back-end/server.js
+++ password-reset/back-end/server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser=require("body-parser");
 const cors = require("cors");
+const connectDB = require ("./config/dbconfig");
 const app = express();
 const port = 3000;
 require("dotenv").config();
+
+connectDB();
 
 app.use(cors());
 // app.use(express.json({ limit: "25mb"}));
@@ -90,5 +93,5 @@ app.post("/send_recovery_email", (req,res) => {
 });
 
 app.listen(port, () => {
-    console.log(`nodemailerProjects is listening at http://localhost:${port}`);
+    console.log(`server is running on port ${port}`);
 });
