1b46b8a6608827fe8162a3225c1f201e59cb6faf
Jayasri 1927 <jayasrig2002@gmail.com>
2025-02-01 12:06:21 +0530

    ...

--- movie-booking system/movie-booking-backend/server.js
+++ movie-booking system/movie-booking-backend/server.js
@@ -10,33 +10,6 @@ require('dotenv').config();
 
 const app = express();
 
-
-// app.post('/api/movies', async (req, res) => {
-//   const { title, genre, releaseDate, showtimes } = req.body;
-
-//   console.log(req.body);  // This logs the body of the request (for debugging)
-
-//   try {
-//     // Create a new movie document based on the received data
-//     const newMovie = new Movie({
-//       title,
-//       genre,
-//       releaseDate,
-//       showtimes,
-//     });
-
-//     // Save the new movie to the database
-//     await newMovie.save();
-
-//     // Send a response with the saved movie data
-//     res.status(201).json(newMovie);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Error saving movie data' });
-//   }
-// });
-
-
 // Session configuration
 app.use(session({
   secret: 'your_session_secret',  // A random string for your session
@@ -50,10 +23,12 @@ app.use(cors());
 
 connectDB();
 
+
 app.use('/api', movieRoutes);
 app.use('/api', bookingRoutes);
 app.use('/api', userRoutes);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
