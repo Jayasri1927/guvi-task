4fe2409d64701b6865cc4dcaabba906603c2375f
Jayasri 1927 <jayasrig2002@gmail.com>
2025-02-20 15:50:49 +0530

    changes

--- book-my-show-clone/backend/server.js
+++ book-my-show-clone/backend/server.js
@@ -24,6 +24,11 @@ app.use("/api/movies", movieRoutes);
 app.use("/api/seats", seatRoutes);
 app.use("/api/bookings", bookingRoutes);
 
+app.get("/", (req, res) => {
+    res.send("Backend is Running...");
+  });
+  
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
