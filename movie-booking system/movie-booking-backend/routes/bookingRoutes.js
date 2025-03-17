9ecab27efc6a1ef5e04b0684aefca8257428fd2b
Jayasri1927 <jayasrig2002@gmail.com>
2025-03-17 13:24:29 +0530

    Delete movie-booking system directory

--- movie-booking system/movie-booking-backend/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { bookTicket } = require('../controllers/bookingController');
-
-router.post('/book', bookTicket);
-
-
-
-module.exports = router;
